a8ef29680b1e4e921d1789fcc2fc7498e1260d3a
JasonKondos <jason.kondos@ucalgary.ca>
2022-12-09 00:37:30 -0700

    Completed setup for structure of project

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
@@ -11,10 +14,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/ElevatorPanelView.fxml"));
+			ElevatorPanelController controller = (ElevatorPanelController)loader.getController();
+			controller.applicationStage = primaryStage;
+			
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Elevator Control Panel");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -25,4 +32,3 @@ public class Main extends Application {
 		launch(args);
 	}
 }
-// https://github.com/JKondos/JKondosElevatorProject.git
